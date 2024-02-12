dc0f05595cf3fb5c12c1380a90c30c8609944b43
alphaura <kylemagri98@gmail.com>
2024-02-12 01:19:53 +0100

    Footer completed + responsive + finishing touches

--- assets/jS/index.js
+++ assets/jS/index.js
@@ -22,3 +22,22 @@ $(document).ready(function(){
         }
     })
   });
+
+
+  window.addEventListener('scroll', reveal);
+
+  function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for ( let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealtTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 120;
+
+        if( revealtTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active') 
+         } else {
+            reveals[i].classList.remove( 'active' );
+         }
+    }
+}
